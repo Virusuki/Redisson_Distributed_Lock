76a085f9cc07dd3ec847ff8a1a8c23a144f2d672
NAM UK KIM <namuk2004@naver.com>
2024-02-09 12:31:28 +0900

    Update CouponService.java

--- src/main/java/com/example/redisson/DeployCoupon/CouponService.java
+++ src/main/java/com/example/redisson/DeployCoupon/CouponService.java
@@ -68,11 +68,11 @@ public class CouponService {
         final int quantity = availableCoupons(key);
 
         if (quantity <= Complete) {
-            log.info("{} / 사용 가능한 커피쿠폰은 모두 소진 (수량 : {}개)", threadName, quantity);
+            log.info("{} - 사용 가능한 커피쿠폰은 모두 소진 (수량 : {}개)", threadName, quantity);
             return;
         }
 
-        log.info("쿠폰 발급 중: {} / 현재 잔여 커피쿠폰{} 수량 : {}개", threadName, keyName, quantity);
+        log.info("쿠폰 발급 중: {} - 현재 잔여 커피쿠폰{} 수량 : {}개", threadName, keyName, quantity);
         setCouponQuantity(key, quantity - 1);
     }
 }
