a81f95cb9f1f12c0e2b7fb256372c5b40baaaa71
NAM UK KIM <namuk2004@naver.com>
2024-02-09 09:10:12 +0900

    Update CouponServiceTest.java

--- src/test/java/com/example/redisson/DeployCoupon/CouponServiceTest.java
+++ src/test/java/com/example/redisson/DeployCoupon/CouponServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
@@ -19,76 +21,109 @@ public class CouponServiceTest {
     @Autowired
     private CouponService couponService;
     private String couponKey;
+    private Exception exception;
 //    private CoffeeCoupon coupon;
-
-    private class UsingCoupon_Lock implements Runnable{
-    	private final String couponKey;
-    	private final CountDownLatch countDownLatch;
-
-    	public UsingCoupon_Lock(String couponKey, CountDownLatch countDownLatch){
-        this.couponKey = couponKey;
-        this.countDownLatch = countDownLatch;
-        }
-
-    	@Override
-    	public void run(){
-           couponService.decreaseCouponWithLock(this.couponKey);
-           countDownLatch.countDown();
-        }
-    }
     
-    private class UsingCoupon_withoutLock implements Runnable{
+    @BeforeEach
+    void CoffeeCoupon_Setup() {
+        final String Menu = "(ICE)Americano";
+        final String code = "code:0001";
+        final int quantity = 100;
+        final CoffeeCoupon coupon = new CoffeeCoupon(Menu, code, quantity);
+
+        this.couponKey = couponService.CodeMaker(coupon.getMenu() ,coupon.getCoffee_code());
+        couponService.setCouponQuantity(this.couponKey, quantity);
+    	}
+ 
+       
+    @Test
+    void 커피쿠폰사용_분산락_적용_테스트() throws InterruptedException {
+        final int numberOfThreads = 115;
+        final CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
+
+        List<Thread> threadList = Stream
+                .generate(() -> new Thread(new UsingCoupon_Lock(this.couponKey, countDownLatch)))
+                .limit(numberOfThreads).collect(Collectors.toList());
+
+        threadList.forEach(Thread::start);
+        
+        threadList.forEach(a -> {
+            try {
+                a.join();
+        
+		    } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        
+        assertThat(exception.getClass()).isEqualTo(IllegalMonitorStateException.class);
+    }  
+    
+
+    private class UsingCoupon_Lock implements Runnable {
         private final String couponKey;
         private final CountDownLatch countDownLatch;
-    		
-        public UsingCoupon_withoutLock(String couponKey, CountDownLatch countDownLatch){
+
+        public UsingCoupon_Lock(String couponKey, CountDownLatch countDownLatch) {
             this.couponKey = couponKey;
             this.countDownLatch = countDownLatch;
         }
 
     	@Override
-    	public void run(){
-            couponService.decreaseCouponWithoutLock(this.couponKey);
-            countDownLatch.countDown();
+        public void run() {
+            try {
+    		    couponService.decreaseCouponWithLock(this.couponKey);
+    	        countDownLatch.countDown();
+    	        countDownLatch.await(); 		    
+    	    
+            } catch (Exception ex) {
+                exception = ex;
+            }
         }
-    }    
-    
-    @BeforeEach
-    void CoffeeCoupon_Setup(){
-        final String Menu = "(ICE)Americano";
-        final String code = "code:0001";
-        final int quantity = 10;
-        final CoffeeCoupon coupon = new CoffeeCoupon(Menu, code, quantity);
-
-        this.couponKey = couponService.CodeMaker(coupon.getMenu() ,coupon.getCoffee_code());
-        couponService.setCouponQuantity(this.couponKey, quantity);
     }
-    
+
     @Test
-    void 커피쿠폰사용_분산락_적용_테스트() throws InterruptedException{
-        final int numberOfThreads = 15;
-        final CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
-
-        List<Thread> threadList = Stream
-                .generate(() -> new Thread(new UsingCoupon_Lock(this.couponKey, countDownLatch)))
-                .limit(numberOfThreads)
-                .collect(Collectors.toList());
-
-        threadList.forEach(Thread::start);
-        countDownLatch.await();
-    }  
-
-    
-    @Test
-    void 커피쿠폰사용_분산락_미적용_테스트() throws InterruptedException{
-        final int numberOfThreads = 15;
+    void 커피쿠폰사용_분산락_미적용_테스트() throws InterruptedException {
+        final int numberOfThreads = 115;
         final CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
 
         List<Thread> threadList = Stream
                 .generate(() -> new Thread(new UsingCoupon_withoutLock(this.couponKey, countDownLatch)))
-                .limit(numberOfThreads)
-                .collect(Collectors.toList());
+                .limit(numberOfThreads).collect(Collectors.toList());
 
         threadList.forEach(Thread::start);
+        
+        threadList.forEach(a -> {
+            try {
+                a.join();
+            
+			} catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        
+        assertThat(exception.getClass()).isEqualTo(IllegalMonitorStateException.class);
     }
+    
+    private class UsingCoupon_withoutLock implements Runnable {
+        private final String couponKey;
+        private final CountDownLatch countDownLatch;
+    		
+        public UsingCoupon_withoutLock(String couponKey, CountDownLatch countDownLatch) {
+            this.couponKey = couponKey;
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void run() {
+            try {
+                couponService.decreaseCouponWithoutLock(this.couponKey);
+                countDownLatch.countDown();	
+            
+			} catch (Exception ex) {
+                exception = ex;
+            }
+        }
+    }    
 }
+
